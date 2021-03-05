70dec3520e2ff88a608b31badcfc30ad5b73c38e
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-03-05 21:51:50 +0100

    projet fini

--- code/index.js
+++ code/index.js
@@ -1,10 +1,23 @@
-var inter = setInterval(change_bg, 1000);
+var inter_bg = setInterval(change_bg, 1000);
+var inter_time = setInterval(change_time, 1000);
 
-function change_bg(){
+function change_bg() {
     var date = new Date();
     var hour = date.getHours();
-    console.log(hour);
-    var opa = (hour)/(24);
-    console.log(hour+' : '+opa);
-    $("#div2").css({opacity : opa});
+
+    var opa = (hour) / 24;
+    console.log('opacity : ' + opa);
+
+    $("#div2").css({opacity: opa});
+}
+
+function change_time(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+
+    console.log('time : '+hour+' : '+minutes);
+
+    $("#hour").text(hour);
+    $("#minute").text(minutes);
 }
